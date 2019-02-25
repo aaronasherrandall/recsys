ad29d6024a3930d7cea68bc10cff6f1f575a9e5a
Aaron Randall <41654628+aaronasherrandall@users.noreply.github.com>
2019-02-26 08:45:11 +0900

    Add files via upload

---
+++ recsys.py
@@ -0,0 +1,31 @@
+from collections import defaultdict, Counter
+
+users_interests = [
+    ["Hadoop", "Big Data", "HBase", "Java", "Spark", "Storm", "Cassandra"],
+    ["NoSQL", "MongoDB", "Cassandra", "HBase", "Postgres"],
+    ["Python", "scikit-learn", "scipy", "numpy", "statsmodels", "pandas"],
+    ["R", "Python", "statistics", "regression", "probability"],
+    ["machine learning", "regression", "decision trees", "libsvm"],
+    ["Python", "R", "Java", "C++", "Haskell", "programming languages"],
+    ["statistics", "probability", "mathematics", "theory"],
+    ["machine learning", "scikit-learn", "Mahout", "neural networks"],
+    ["neural networks", "deep learning", "Big Data", "artificial intelligence"],
+    ["Hadoop", "Java", "MapReduce", "Big Data"],
+    ["statistics", "R", "statsmodels"],
+    ["C++", "deep learning", "artificial intelligence", "probability"],
+    ["pandas", "R", "Python"],
+    ["databases", "HBase", "Postgres", "MySQL", "MongoDB"],
+    ["libsvm", "regression", "support vector machines"]
+]
+
+popular_interests = Counter(interest
+                            for users_interests in users_interests
+                            for interest in users_interests).most_common()
+
+def most_popular_new_interests(users_interests, max_results=5):
+    suggestions = [(interest, frequency)
+                    for interest, frequency in popular_interests
+                    if interest not in users_interests]
+    return suggestions[:max_results]
+
+print(most_popular_new_interests(users_interests[5], 5))
